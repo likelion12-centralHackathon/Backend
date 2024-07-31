c5b420bbd67dd9625bc03e499e4c9ae697ed7111
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-31 21:04:23 +0900

    feat: Timer 저장 요청 dto 추가 (#3)

---
+++ src/main/java/com/likelion/timer/domain/Timer/dto/req/PartReqDto.java
@@ -0,0 +1,23 @@
+package com.likelion.timer.domain.Timer.dto.req;
+
+import static com.likelion.timer.domain.Timer.constants.TimerConstants.*;
+
+import java.util.List;
+
+import com.likelion.timer.domain.model.PartTypeEnum;
+import com.likelion.timer.global.validation.MaxListSize;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PartReqDto {
+
+	@MaxListSize(max = MAX_PARTS)
+	List<PartTypeEnum> partTypes;
+}
---
+++ src/main/java/com/likelion/timer/domain/Timer/dto/req/TimerReqDto.java
@@ -0,0 +1,36 @@
+package com.likelion.timer.domain.Timer.dto.req;
+
+import static com.likelion.timer.domain.Timer.constants.TimerConstants.*;
+
+import java.util.List;
+
+import com.likelion.timer.global.validation.MaxListSize;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class TimerReqDto {
+
+	@NotNull
+	Boolean isPermanent;
+
+	@NotNull
+	@DecimalMin(value = "0.0", message = "값은 0.0 이상이어야 합니다.")
+	@DecimalMax(value = "5.0", message = "값은 5.0 이하이어야 합니다.")
+	private float cycle;
+
+	@NotNull
+	Boolean isSettingByUser;
+
+	@MaxListSize(max = MAX_STRETCHING_TIMES)
+	List<PartReqDto> parts;
+}
