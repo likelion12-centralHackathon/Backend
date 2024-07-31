58246ee381a198d1c20557ad296ec02e89914dc5
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-31 21:11:36 +0900

    feat: Timer 관련 error 코드 추가 (#3)

---
+++ src/main/java/com/likelion/timer/domain/Timer/error/TimerErrorCode.java
@@ -0,0 +1,25 @@
+package com.likelion.timer.domain.Timer.error;
+
+import org.springframework.http.HttpStatus;
+
+import com.likelion.timer.global.error.ErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public enum TimerErrorCode implements ErrorCode {
+	INVALID_TIMER_STATE(HttpStatus.BAD_REQUEST, "해당하는 타이머 상태가 없습니다."),
+	INVALID_PART_TYPE(HttpStatus.BAD_REQUEST, "해당하는 스트레칭 부위가 없습니다."),
+	INVALID_PART_LIST_BY_USER(HttpStatus.BAD_REQUEST, "직접 설정인 경우, 스트레칭 부위 리스트 정보가 필요합니다."),
+	INVALID_PART_LIST_BY_SYSTEM(HttpStatus.BAD_REQUEST, "랜덤 설정인 경우, 스트레칭 부위 리스트 정보가 필요하지 않습니다."),
+
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	TimerErrorCode(HttpStatus httpStatus, String message) {
+		this.httpStatus = httpStatus;
+		this.message = message;
+	}
+}
