8c4d9e47cd940189c7d37994633c781961ce6d98
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-01 20:15:07 +0900

    feat: 타이머 이름 설정 에러 추가 (#3)

--- src/main/java/com/likelion/timer/domain/Timer/error/TimerErrorCode.java
+++ src/main/java/com/likelion/timer/domain/Timer/error/TimerErrorCode.java
@@ -10,6 +10,8 @@ import lombok.Getter;
 public enum TimerErrorCode implements ErrorCode {
 	INVALID_TIMER_STATE(HttpStatus.BAD_REQUEST, "해당하는 타이머 상태가 없습니다."),
 	INVALID_PART_TYPE(HttpStatus.BAD_REQUEST, "해당하는 스트레칭 부위가 없습니다."),
+	INVALID_TIMER_NAME(HttpStatus.BAD_REQUEST, "타이머 이름은 중복될 수 없습니다."),
+	TIMER_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "타이머 이름 설정이 필요합니다."),
 	INVALID_PART_LIST_BY_USER(HttpStatus.BAD_REQUEST, "직접 설정인 경우, 스트레칭 부위 리스트 정보가 필요합니다."),
 	INVALID_PART_LIST_BY_SYSTEM(HttpStatus.BAD_REQUEST, "랜덤 설정인 경우, 스트레칭 부위 리스트 정보가 필요하지 않습니다."),
 
