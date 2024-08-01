7909879ef59d143628b4fd6497e3ae34c148aade
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-01 20:11:40 +0900

    remove: 불필요한 repository 메소드 제거 (#3)

--- src/main/java/com/likelion/timer/domain/Timer/domain/repository/TimerRepository.java
+++ src/main/java/com/likelion/timer/domain/Timer/domain/repository/TimerRepository.java
@@ -1,5 +1,7 @@
 package com.likelion.timer.domain.Timer.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.likelion.timer.domain.Timer.domain.entity.Timer;
 @Repository
 public interface TimerRepository extends JpaRepository<Timer, Long> {
 
+	Optional<Timer> findByName(String name);
 }
