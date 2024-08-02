166286f71c42505f82f7a19201616f8453ffaa3a
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-02 18:12:10 +0900

    fix: join column 지정을 통한 매핑으로 수정  (#3)

--- src/main/java/com/likelion/timer/domain/Timer/domain/entity/Timer.java
+++ src/main/java/com/likelion/timer/domain/Timer/domain/entity/Timer.java
@@ -21,6 +21,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Builder;
@@ -39,6 +40,7 @@ public class Timer extends BaseTime {
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 	private String name;
