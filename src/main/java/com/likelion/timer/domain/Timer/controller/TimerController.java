4462a84e746a76784f538e7f8f003900fde55100
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-02 09:54:00 +0900

    feat: 타이머 상세, 리스트 조회, 타이머 상태, 설정 수정, 삭제 api 추가 (#3)

--- src/main/java/com/likelion/timer/domain/Timer/controller/TimerController.java
+++ src/main/java/com/likelion/timer/domain/Timer/controller/TimerController.java
@@ -1,14 +1,24 @@
 package com.likelion.timer.domain.Timer.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.likelion.timer.domain.Timer.dto.req.TimerReqDto;
+import com.likelion.timer.domain.Timer.dto.req.TimerUpdateReqDto;
+import com.likelion.timer.domain.Timer.dto.res.TimerListResDto;
 import com.likelion.timer.domain.Timer.dto.res.TimerResDto;
 import com.likelion.timer.domain.Timer.service.TimerService;
+import com.likelion.timer.domain.model.TimerStateTypeEnum;
 import com.likelion.timer.global.common.DataResponseDto;
 import com.likelion.timer.global.common.ResponseDto;
 
@@ -31,4 +41,44 @@ public class TimerController {
 		return ResponseEntity.status(201).body(DataResponseDto.of(timerResDto, 201));
 	}
 
+	@PatchMapping("/{timerId}")
+	public ResponseEntity<ResponseDto> updateTimerState(@PathVariable(name = "timerId") Long timerId,
+		@RequestParam TimerStateTypeEnum timerStateType) {
+		timerService.updateTimerState(timerId, timerStateType);
+
+		return ResponseEntity.ok(ResponseDto.of(200));
+	}
+
+	@DeleteMapping("/{timerId}")
+	public ResponseEntity<ResponseDto> deleteTimer(@PathVariable(name = "timerId") Long timerId) {
+		timerService.deleteTimer(timerId);
+
+		return ResponseEntity.ok(ResponseDto.of(200));
+	}
+
+	@PostMapping("/{timerId}")
+	public ResponseEntity<ResponseDto> updateTimer(@PathVariable(name = "timerId") Long timerId,
+		@RequestBody @Valid TimerUpdateReqDto timerUpdateReqDto) {
+		TimerResDto timerResDto = timerService.updateTimer(timerId, timerUpdateReqDto);
+
+		return ResponseEntity.status(200).body(DataResponseDto.of(timerResDto, 200));
+	}
+
+	@GetMapping
+	public ResponseEntity<ResponseDto> getTimerList() {
+		List<TimerListResDto> timerList = timerService.getTimerList();
+
+		if (timerList.isEmpty()) {
+			return ResponseEntity.status(204).body(DataResponseDto.of("저장된 타이머가 없습니다.", 204));
+		} else {
+			return ResponseEntity.status(200).body(DataResponseDto.of(timerList, 200));
+		}
+	}
+
+	@GetMapping("/{timerId}")
+	public ResponseEntity<ResponseDto> getTimer(@PathVariable(name = "timerId") Long timerId) {
+		TimerResDto timerResDto = timerService.getTimer(timerId);
+
+		return ResponseEntity.status(200).body(DataResponseDto.of(timerResDto, 200));
+	}
 }
--- src/main/java/com/likelion/timer/domain/Timer/service/TimerService.java
+++ src/main/java/com/likelion/timer/domain/Timer/service/TimerService.java
@@ -12,11 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 import com.likelion.timer.domain.Timer.domain.entity.PartList;
 import com.likelion.timer.domain.Timer.domain.entity.Timer;
 import com.likelion.timer.domain.Timer.domain.repository.TimerRepository;
+import com.likelion.timer.domain.Timer.dto.req.PartReqDto;
 import com.likelion.timer.domain.Timer.dto.req.TimerReqDto;
+import com.likelion.timer.domain.Timer.dto.req.TimerUpdateReqDto;
+import com.likelion.timer.domain.Timer.dto.res.TimerListResDto;
 import com.likelion.timer.domain.Timer.dto.res.TimerResDto;
 import com.likelion.timer.domain.Timer.error.TimerErrorCode;
 import com.likelion.timer.domain.User.domain.T_User;
 import com.likelion.timer.domain.User.domain.T_UserRepository;
+import com.likelion.timer.domain.model.TimerStateTypeEnum;
 import com.likelion.timer.global.error.exception.AppException;
 
 import lombok.RequiredArgsConstructor;
@@ -32,44 +36,83 @@ public class TimerService {
 
 	private static final Random RANDOM = new Random();
 
+	@Transactional(readOnly = true)
+	public TimerResDto getTimer(Long timerId) {
+		// TODO user 연결
+		Timer timer = timerRepository.findById(timerId)
+			.orElseThrow(() -> new AppException(TimerErrorCode.TIMER_NOT_FOUND));
+
+		return TimerResDto.fromEntity(timer);
+	}
+
+	@Transactional(readOnly = true)
+	public List<TimerListResDto> getTimerList() {
+		// TODO user 연결
+		return timerRepository.findPermanentTimersNameAndIdByUserId(1L);
+	}
+
+	@Transactional
+	public void updateTimerState(Long timerId, TimerStateTypeEnum timerState) {
+		// 사용자 찾기
+		T_User user = T_User.builder().name("name").build();
+		userRepository.save(user);
+
+		Timer timer = timerRepository.findById(timerId)
+			.orElseThrow(() -> new AppException(TimerErrorCode.TIMER_NOT_FOUND));
+
+		timer.changedTimerState(timerState);
+
+		if (!timer.getIsPermanent() && timerState.equals(TimerStateTypeEnum.TIMER_DESTROY)) {
+			timerRepository.delete(timer);
+		} else {
+			timerRepository.save(timer);
+		}
+	}
+
+	@Transactional
+	public void deleteTimer(Long timerId) {
+		Timer timer = timerRepository.findById(timerId)
+			.orElseThrow(() -> new AppException(TimerErrorCode.TIMER_NOT_FOUND));
+		timerRepository.delete(timer);
+	}
+
+	@Transactional
+	public TimerResDto updateTimer(Long timerId, TimerUpdateReqDto timerUpdateReqDto) {
+		// 사용자 찾기
+		T_User user = T_User.builder().name("name").build();
+		userRepository.save(user);
+
+		Timer timer = timerRepository.findById(timerId)
+			.orElseThrow(() -> new AppException(TimerErrorCode.TIMER_NOT_FOUND));
+
+		// name 중복 체크
+		if (!timer.getName().equals(timerUpdateReqDto.getName())) {
+			checkUniqueName(user.getId(), timerUpdateReqDto.getName());
+		}
+
+		// partList 설정
+		List<PartList> partLists = checkPartLists(timerUpdateReqDto.getIsSettingByUser(), timerUpdateReqDto.getParts());
+
+		// timer 객체 생성
+		timer.updateTimer(timerUpdateReqDto, partLists);
+
+		// timer 저장
+		timerRepository.save(timer);
+
+		return TimerResDto.fromEntity(timer);
+	}
+
 	@Transactional
 	public TimerResDto addTimer(TimerReqDto timerReqDto) {
 		// 사용자 찾기
 		T_User user = T_User.builder().name("name").build();
 		userRepository.save(user);
 
-		// partList 설정
-		List<PartList> partLists;
-
-		if (timerReqDto.getIsSettingByUser()) { // 직접 설정인 경우
-			if (timerReqDto.getParts() == null) { // 스틑레칭 부위가 안 온 경우
-				throw new AppException(TimerErrorCode.INVALID_PART_LIST_BY_USER);
-			}
-
-			partLists = partListService.saveParts(timerReqDto.getParts());
-
-		} else { // 랜덤인 경우
-			if (timerReqDto.getParts() != null) { // 스틑레칭 부위가 온 경우
-				throw new AppException(TimerErrorCode.INVALID_PART_LIST_BY_SYSTEM);
-			}
-
-			partLists = partListService.saveRandomUniqueParts(RANDOM.nextInt(MAX_STRETCHING_TIMES) + 1);
-		}
-
 		// name 설정
-		String name;
-		if (timerReqDto.getIsPermanent() && timerReqDto.getName() == null) {
-			throw new AppException(TimerErrorCode.TIMER_NAME_REQUIRED);
-		} else if (!timerReqDto.getIsPermanent()) {
-			name = null;
-		} else {
-			Optional<Timer> timer = timerRepository.findByName(timerReqDto.getName());
-			if (timer.isEmpty()) {
-				name = timerReqDto.getName();
-			} else {
-				throw new AppException(TimerErrorCode.INVALID_TIMER_NAME);
-			}
-		}
+		String name = checkName(timerReqDto.getIsPermanent(), user.getId(), timerReqDto.getName());
+
+		// partList 설정
+		List<PartList> partLists = checkPartLists(timerReqDto.getIsSettingByUser(), timerReqDto.getParts());
 
 		// timer 객체 생성
 		Timer timer = Timer.builder()
@@ -85,6 +128,46 @@ public class TimerService {
 		timerRepository.save(timer);
 
 		return TimerResDto.fromEntity(timer);
+	}
+
+	private List<PartList> checkPartLists(Boolean isSettingByUser, List<PartReqDto> parts) {
+		List<PartList> partLists;
+
+		if (isSettingByUser) { // 직접 설정인 경우
+			if (parts == null) { // 스틑레칭 부위가 안 온 경우
+				throw new AppException(TimerErrorCode.INVALID_PART_LIST_BY_USER);
+			}
+
+			partLists = partListService.saveParts(parts);
+
+		} else { // 랜덤인 경우
+			if (parts != null) { // 스틑레칭 부위가 온 경우
+				throw new AppException(TimerErrorCode.INVALID_PART_LIST_BY_SYSTEM);
+			}
+
+			partLists = partListService.saveRandomUniqueParts(RANDOM.nextInt(MAX_STRETCHING_TIMES) + 1);
+		}
+
+		return partLists;
+	}
+
+	private String checkName(Boolean isPermanent, Long userId, String name) {
+		if (isPermanent && name == null) {
+			throw new AppException(TimerErrorCode.TIMER_NAME_REQUIRED);
+		} else if (isPermanent && checkUniqueName(userId, name)) {
+			return name;
+		} else {
+			return null;
+		}
+	}
+
+	private Boolean checkUniqueName(Long userId, String name) {
+		Optional<Timer> timer = timerRepository.findByUserIdAndName(userId, name);
+		if (timer.isEmpty()) {
+			return true;
+		} else {
+			throw new AppException(TimerErrorCode.INVALID_TIMER_NAME);
+		}
 	}
 
 }
