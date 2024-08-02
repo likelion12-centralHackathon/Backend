f38683ffa64f99fae57b48ca8762b5e9bcfe4bd8
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-02 09:48:39 +0900

    feat: 유저 기준 수정, 리스트 가져오는 jpa 추가 (#3)

--- src/main/java/com/likelion/timer/domain/Timer/domain/repository/TimerRepository.java
+++ src/main/java/com/likelion/timer/domain/Timer/domain/repository/TimerRepository.java
@@ -1,14 +1,25 @@
 package com.likelion.timer.domain.Timer.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.likelion.timer.domain.Timer.domain.entity.Timer;
+import com.likelion.timer.domain.Timer.dto.res.TimerListResDto;
 
 @Repository
 public interface TimerRepository extends JpaRepository<Timer, Long> {
 
-	Optional<Timer> findByName(String name);
+	@Query("SELECT t FROM Timer t WHERE t.user.id = :userId AND t.name = :name")
+	Optional<Timer> findByUserIdAndName(@Param("userId") Long userId, @Param("name") String name);
+
+	@Query("SELECT new com.likelion.timer.domain.Timer.dto.res.TimerListResDto(t.id, t.name) " +
+		"FROM Timer t "
+		+ "WHERE t.user.id = :userId AND t.isPermanent = true " +
+		"ORDER BY t.regTime DESC")
+	List<TimerListResDto> findPermanentTimersNameAndIdByUserId(@Param("userId") Long userId);
 }
