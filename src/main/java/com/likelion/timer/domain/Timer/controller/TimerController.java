38a5407fe2acb8a783eb3e3bcc48e1eea327868d
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-03 23:32:32 +0900

    feat: 일자별 timer 통계, timer 순서 조회 api, 타이머 fcm 스케줄링 추가 (#9)

--- src/main/java/com/likelion/timer/domain/Timer/controller/TimerController.java
+++ src/main/java/com/likelion/timer/domain/Timer/controller/TimerController.java
@@ -1,26 +1,31 @@
 package com.likelion.timer.domain.Timer.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.likelion.timer.domain.Timer.dto.req.RecordTimerReqDto;
 import com.likelion.timer.domain.Timer.dto.req.TimerReqDto;
 import com.likelion.timer.domain.Timer.dto.req.TimerUpdateReqDto;
+import com.likelion.timer.domain.Timer.dto.res.PartStaticResDto;
+import com.likelion.timer.domain.Timer.dto.res.RecordTimerResDto;
 import com.likelion.timer.domain.Timer.dto.res.TimerListResDto;
+import com.likelion.timer.domain.Timer.dto.res.TimerPartListResDto;
 import com.likelion.timer.domain.Timer.dto.res.TimerResDto;
+import com.likelion.timer.domain.Timer.service.RecordTimerService;
 import com.likelion.timer.domain.Timer.service.TimerService;
-import com.likelion.timer.domain.model.TimerStateTypeEnum;
 import com.likelion.timer.global.common.DataResponseDto;
 import com.likelion.timer.global.common.ResponseDto;
 
@@ -35,6 +40,29 @@ import lombok.extern.slf4j.Slf4j;
 public class TimerController {
 
 	private final TimerService timerService;
+	private final RecordTimerService recordTimerService;
+
+	@GetMapping("/search/{date}")
+	public ResponseEntity<ResponseDto> getPartListsByDate(Authentication authentication,
+		@PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
+		UserDetails userDetails = (UserDetails)authentication.getPrincipal();
+		List<TimerPartListResDto> timerPartListResDtos = recordTimerService.getPartListsByDate(
+			userDetails.getUsername(), date);
+
+		return ResponseEntity.status(201).body(DataResponseDto.of(timerPartListResDtos, 201));
+
+	}
+
+	@GetMapping("/static/{date}")
+	public ResponseEntity<ResponseDto> getPartStaticsByDate(Authentication authentication,
+		@PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+		UserDetails userDetails = (UserDetails)authentication.getPrincipal();
+		List<PartStaticResDto> partStaticResDtos = recordTimerService.getPartStaticsByDate(
+			userDetails.getUsername(), date);
+
+		return ResponseEntity.status(201).body(DataResponseDto.of(partStaticResDtos, 201));
+	}
 
 	@PostMapping
 	public ResponseEntity<ResponseDto> addTimer(Authentication authentication,
@@ -45,13 +73,14 @@ public class TimerController {
 		return ResponseEntity.status(201).body(DataResponseDto.of(timerResDto, 201));
 	}
 
-	@PatchMapping("/{timerId}")
-	public ResponseEntity<ResponseDto> updateTimerState(Authentication authentication,
-		@PathVariable(name = "timerId") Long timerId, @RequestParam TimerStateTypeEnum timerStateType) {
+	@PostMapping("/state")
+	public ResponseEntity<ResponseDto> updateTimerStateAndAddRecordTimer(Authentication authentication,
+		@Valid @RequestBody RecordTimerReqDto recordTimerReqDto) {
 		UserDetails userDetails = (UserDetails)authentication.getPrincipal();
-		timerService.updateTimerState(userDetails.getUsername(), timerId, timerStateType);
+		RecordTimerResDto recordTimerResDto = recordTimerService.addRecordTimer(userDetails.getUsername(),
+			recordTimerReqDto);
 
-		return ResponseEntity.ok(ResponseDto.of(200));
+		return ResponseEntity.status(201).body(DataResponseDto.of(recordTimerResDto, 201));
 	}
 
 	@DeleteMapping("/{timerId}")
@@ -91,5 +120,10 @@ public class TimerController {
 		TimerResDto timerResDto = timerService.getTimer(userDetails.getUsername(), timerId);
 
 		return ResponseEntity.status(200).body(DataResponseDto.of(timerResDto, 200));
+	}
+
+	@Scheduled(fixedRate = 1000)
+	public void checkTimers() {
+		timerService.checkAndSendNotifications();
 	}
 }
