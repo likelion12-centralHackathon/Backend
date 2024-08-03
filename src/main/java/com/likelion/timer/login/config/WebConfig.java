ee12915e52061b237f29df2c8a659fa7aec7ae9c
jung <dnjswnddkgg@naver.com>
2024-08-03 23:18:13 +0900

    chore: fix youtube list feature

--- src/main/java/com/likelion/timer/login/config/WebConfig.java
+++ src/main/java/com/likelion/timer/login/config/WebConfig.java
@@ -1,8 +1,5 @@
 package com.likelion.timer.login.config;
 
-
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +9,17 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
+		// /api/v1/Youtube/** 경로에 대한 CORS 설정
 		registry.addMapping("/api/v1/Youtube/**")
-			.allowedOrigins("http://localhost:8080") // 클라이언트의 도메인
+			.allowedOrigins("http://localhost:8080")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+
+		// /api/** 경로에 대한 CORS 설정
+		registry.addMapping("/api/**")
+			.allowedOrigins("http://localhost:3000") // 다른 클라이언트의 도메인
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true);
 	}
