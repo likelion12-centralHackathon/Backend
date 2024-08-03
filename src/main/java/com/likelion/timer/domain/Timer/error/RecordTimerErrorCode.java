38d7904d22d8b9f5288cd69327bfc0f1522cd2fe
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-03 23:32:50 +0900

    feat: 관련 에러 메세지 추가 (#9)

---
+++ src/main/java/com/likelion/timer/domain/Timer/error/RecordTimerErrorCode.java
@@ -0,0 +1,25 @@
+package com.likelion.timer.domain.Timer.error;
+
+import org.springframework.http.HttpStatus;
+
+import com.likelion.timer.global.error.ErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public enum RecordTimerErrorCode implements ErrorCode {
+	INVALID_NOTI_STATE(HttpStatus.BAD_REQUEST, "해당하는 알림 종류가 없습니다."),
+	INVALID_TIMER_RESTART(HttpStatus.BAD_REQUEST, "중지한 적이 없는 경우, 재시작을 할 수 없습니다."),
+	INVALID_STRETCHING_DONE_STATE(HttpStatus.BAD_REQUEST, "스트레칭을 시작해야 종료할 수 있습니다."),
+	INVALID_STRETCHING_DONE_PART(HttpStatus.BAD_REQUEST, "시작한 스트레칭 부위와 일치하지 않습니다."),
+	NOT_FOUND_TIMER_RECORD(HttpStatus.NOT_FOUND, "Timer 이전 기록을 찾지 못했습니다."),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	RecordTimerErrorCode(HttpStatus httpStatus, String message) {
+		this.httpStatus = httpStatus;
+		this.message = message;
+	}
+}
--- src/main/java/com/likelion/timer/domain/Timer/error/TimerErrorCode.java
+++ src/main/java/com/likelion/timer/domain/Timer/error/TimerErrorCode.java
@@ -8,8 +8,14 @@ import lombok.Getter;
 
 @Getter
 public enum TimerErrorCode implements ErrorCode {
+	RECORD_TIMER_NOT_FOUND(HttpStatus.NOT_FOUND, "시작된 타이머가 아닙니다."),
 	TIMER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 타이머가 없습니다."),
+	TIMER_NOT_RUN(HttpStatus.BAD_REQUEST, "타이머가 돌아가는 상태가 아닙니다."),
+	INVALID_TIMER_TO_CHANGE_RUN(HttpStatus.BAD_REQUEST, "타이머가 돌아가는 상태에서 또 시작할 수 없습니다."),
+	INVALID_TIMER_RESTART(HttpStatus.BAD_REQUEST, "시작된 상태에서 또 재시작을 할 수는 없습니다."),
+	INVALID_STRETCHING_TO_TIMER(HttpStatus.BAD_REQUEST, "스트레칭이 시작된 상태에서 스트레칭 종료 외의 다른 설정을 할 수 없습니다."),
 	INVALID_TIMER_STATE(HttpStatus.BAD_REQUEST, "해당하는 타이머 상태가 없습니다."),
+	INVALID_PART_STATE_TYPE(HttpStatus.BAD_REQUEST, "해당하는 스트레칭 상태가 없습니다."),
 	INVALID_PART_TYPE(HttpStatus.BAD_REQUEST, "해당하는 스트레칭 부위가 없습니다."),
 	INVALID_TIMER_NAME(HttpStatus.BAD_REQUEST, "타이머 이름은 중복될 수 없습니다."),
 	TIMER_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "타이머 이름 설정이 필요합니다."),
