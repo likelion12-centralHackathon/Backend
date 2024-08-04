2401dec2e0b834e712013eca85074f4ec537e328
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-04 11:03:19 +0900

    remove: 불필요한 응답 필드 제거 (#15)

--- src/main/java/com/likelion/timer/domain/Timer/dto/res/TimerResDto.java
+++ src/main/java/com/likelion/timer/domain/Timer/dto/res/TimerResDto.java
@@ -3,7 +3,6 @@ package com.likelion.timer.domain.Timer.dto.res;
 import java.util.List;
 
 import com.likelion.timer.domain.Timer.domain.entity.Timer;
-import com.likelion.timer.domain.model.TimerStateTypeEnum;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -14,8 +13,6 @@ public class TimerResDto {
 	private Long timerId;
 	private String name;
 	private float cycle;
-	private TimerStateTypeEnum timerState;
-	private boolean isPermanent;
 	private boolean isSettingByUser;
 	private List<PartListResDto> parts;
 
@@ -25,10 +22,7 @@ public class TimerResDto {
 		return TimerResDto.builder()
 			.timerId(timer.getId())
 			.name(timer.getName())
-			.timerState(timer.getTimerState())
 			.cycle(cycle)
-			.isSettingByUser(timer.getIsSettingByUser())
-			.isPermanent(timer.getIsPermanent())
 			.isSettingByUser(timer.getIsSettingByUser())
 			.parts(timer.getPartLists().stream()
 				.map(PartListResDto::fromEntity)
